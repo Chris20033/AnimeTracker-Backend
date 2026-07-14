ed72b0e34b430913852a21a403f24c7e7344c903
Chris20033 <capimarquez23@gmail.com>
2026-07-13 20:23:09 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { env } = require('./config/env');
 
-app.listen(env.port, () => {
+app.listen(env.port, '0.0.0.0', () => {
   console.log(`AnimeTracker API running on http://localhost:${env.port}
 AnimeTracker Swagger: http://localhost:${env.port}/api/docs`);
 });
